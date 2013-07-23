69f42c2dd8fa672b32d0d985c4c0cdccd14e8ae8
mue <mue.miv@gmail.com>
2013-07-23 22:54:27 +0300

    initial version

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+
+
+  // Project configuration.
+  grunt.initConfig({
+
+    // Configuration to be run (and then tested).
+    stylus: {
+	  compile: {
+		options: {
+		  compress: false
+		},
+		files: {
+		  'styles/style.css': 'stylus/*.styl',
+		}
+	  }
+	},
+	watch: {
+		stylus: {
+			files: 'stylus/*.styl',
+			tasks: ['stylus'],
+			options: {
+			  debounceDelay: 250
+			}
+		}
+	}
+	
+  });
+
+	grunt.loadNpmTasks('grunt-contrib-stylus');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['watch']);
+
+};
