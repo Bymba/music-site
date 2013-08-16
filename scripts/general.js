46c7851133a569dafd90c29743f8ba2aa257898b
mue <mue.miv@gmail.com>
2013-08-16 15:50:48 +0300

    contacts added

--- scripts/general.js
+++ scripts/general.js
@@ -19,6 +19,13 @@ jQuery(document).ready(function(){
 
     
     /* some custom settings */
+    $('.iosSliderSmal').iosSlider({
+        desktopClickDrag: true,
+        snapToChildren: true,
+        infiniteSlider: true,
+        navNextSelector: '.nextSm',
+        navPrevSelector: '.prevSm'
+    });
     $('.iosSlider').iosSlider({
         desktopClickDrag: true,
         snapToChildren: true,
@@ -74,6 +81,23 @@ jQuery(document).ready(function(){
         defaultTab : 'li#tab-2',
         transitionIn : 'slideDown'
     });
+
+
+    //google map
+
+    function initialize() {
+        var map_canvas = document.getElementById('map_canvas');
+        var map_options = {
+          center: new google.maps.LatLng(50.442047, 30.626659),
+          zoom: 12,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(map_canvas, map_options)
+        }
+        google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
 
 });    
 
